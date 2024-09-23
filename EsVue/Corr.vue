68db169ba17607162de46287b1f179a22f0dd348
Mirko Campari <82829377+MaSTERmIKK@users.noreply.github.com>
2024-09-23 17:31:53 +0200

    Create Corr.vue

---
+++ EsVue/Corr.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <Login />
+  </div>
+</template>
+
+<script>
+import Login from './components/Login.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Login,
+  },
+};
+</script>
+
+<style>
+/* Stili globali se necessari */
+</style>
